54313965c0a82a8bdc417045169e9d5893a12cd0
Sunny <sunny.s@flipkart.com>
2018-01-12 21:42:57 +0530

    Add constructor to GameInitImpl

--- FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/game/init/GameInitializerImpl.java
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/game/init/GameInitializerImpl.java
@@ -1,11 +1,24 @@
 package com.stunapps.fearlessjumper.game.init;
 
+import com.google.inject.Inject;
+import com.stunapps.fearlessjumper.component.GameComponentManager;
+import com.stunapps.fearlessjumper.entity.EntityManager;
+
 /**
  * Created by sunny.s on 12/01/18.
  */
 
 public class GameInitializerImpl implements GameInitializer
 {
+    private final EntityManager entityManager;
+    private final GameComponentManager gameComponentManager;
+
+    @Inject
+    public GameInitializerImpl(EntityManager entityManager, GameComponentManager gameComponentManager)
+    {
+        this.entityManager = entityManager;
+        this.gameComponentManager = gameComponentManager;
+    }
 
     @Override
     public void initialise()
