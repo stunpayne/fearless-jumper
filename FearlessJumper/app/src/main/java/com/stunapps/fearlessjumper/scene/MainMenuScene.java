8c1480c0267f331c9fe94fc9cdf18667e3258862
Sunny <sunny.s@flipkart.com>
2018-02-11 15:45:29 +0530

    CPCHEC-0001 Move friction handling to separate method

---
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/scene/MainMenuScene.java
@@ -0,0 +1,35 @@
+package com.stunapps.fearlessjumper.scene;
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+/**
+ * Created by sunny.s on 11/02/18.
+ */
+
+public class MainMenuScene implements Scene
+{
+	@Override
+	public void update()
+	{
+
+	}
+
+	@Override
+	public void draw(Canvas canvas)
+	{
+
+	}
+
+	@Override
+	public void terminate()
+	{
+
+	}
+
+	@Override
+	public void receiveTouch(MotionEvent motionEvent)
+	{
+
+	}
+}
--- FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/system/update/PhysicsSystem.java
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/system/update/PhysicsSystem.java
@@ -20,133 +20,140 @@ import static com.stunapps.fearlessjumper.helper.Constants.scale;
 @Singleton
 public class PhysicsSystem implements UpdateSystem, CollisionListener
 {
-    private final ComponentManager componentManager;
-    private static long lastProcessTime = System.nanoTime();
+	private final ComponentManager componentManager;
+	private static long lastProcessTime = System.nanoTime();
 
-    private static final float GRAVITY = -9.8f;
+	private static final float GRAVITY = -9.8f;
 
-    @Inject
-    public PhysicsSystem(ComponentManager componentManager, CollisionSystem collisionSystem)
-    {
-        this.componentManager = componentManager;
-        collisionSystem.registerObserver(this);
-    }
+	@Inject
+	public PhysicsSystem(ComponentManager componentManager, CollisionSystem collisionSystem)
+	{
+		this.componentManager = componentManager;
+		collisionSystem.registerObserver(this);
+	}
 
-    @Override
-    public void process(long deltaTime)
-    {
-        lastProcessTime = System.currentTimeMillis();
+	@Override
+	public void process(long deltaTime)
+	{
+		lastProcessTime = System.currentTimeMillis();
 
-        Set<Entity> physicalEntities = componentManager.getEntities(PhysicsComponent.class);
+		Set<Entity> physicalEntities = componentManager.getEntities(PhysicsComponent.class);
 
-        for (Entity entity : physicalEntities)
-        {
-            PhysicsComponent physicsComponent = entity.getComponent(PhysicsComponent.class);
-            if (physicsComponent.mass > 0 && physicsComponent.applyGravity)
-            {
-                applyGravity(entity, deltaTime);
-            }
-        }
-    }
+		for (Entity entity : physicalEntities)
+		{
+			PhysicsComponent physicsComponent = entity.getComponent(PhysicsComponent.class);
+			if (physicsComponent.mass > 0 && physicsComponent.applyGravity)
+			{
+				applyGravity(entity, deltaTime);
+			}
+		}
+	}
 
-    @Override
-    public long getLastProcessTime()
-    {
-        return lastProcessTime;
-    }
+	@Override
+	public long getLastProcessTime()
+	{
+		return lastProcessTime;
+	}
 
-    private void applyGravity(Entity entity, long deltaTime)
-    {
-        PhysicsComponent physicsComponent = entity.getComponent(
-                PhysicsComponent.class);
-        physicsComponent.velocity.y -= (GRAVITY * scale() * deltaTime / ONE_SECOND_NANOS);
-    }
+	private void applyGravity(Entity entity, long deltaTime)
+	{
+		PhysicsComponent physicsComponent = entity.getComponent(PhysicsComponent.class);
+		physicsComponent.velocity.y -= (GRAVITY * scale() * deltaTime / ONE_SECOND_NANOS);
+	}
 
-    @Override
-    public void onCollision(Entity entity1, Entity entity2, CollisionResponse collisionResponse,
-            long deltaTime)
-    {
-        if (entity1.hasComponent(PhysicsComponent.class) && entity2.hasComponent(
-                PhysicsComponent.class))
-        {
-            switch ((collisionResponse.collisionFace))
-            {
-                case HORIZONTAL:
-                    resolveHorizontalFriction(entity1, entity2, deltaTime);
-                    break;
-                case HORIZONTAL_REVERSE:
-                    break;
-                case VERTICAL:
-                    resolveVerticalFriction(entity1, entity2, deltaTime);
-                    break;
-                case VERTICAL_REVERSE:
-                    break;
-            }
-        }
-    }
+	@Override
+	public void onCollision(Entity entity1, Entity entity2, CollisionResponse collisionResponse,
+			long deltaTime)
+	{
+		if (entity1.hasComponent(PhysicsComponent.class) && entity2.hasComponent(
+				PhysicsComponent.class))
+		{
+			handleFriction(entity1, entity2, collisionResponse, deltaTime);
+		}
+	}
 
-    private void resolveHorizontalFriction(Entity entity1, Entity entity2, long deltaTime)
-    {
-        PhysicsComponent physicsComponent1 = entity1.getComponent(PhysicsComponent.class);
-        PhysicsComponent physicsComponent2 = entity2.getComponent(PhysicsComponent.class);
-        float friction = physicsComponent1.friction.bottomFriction + physicsComponent2.friction.topFriction;
-        float entity1XVel = Math.abs(physicsComponent1.velocity.x);
-        if (entity1XVel > 0)
-        {
-            entity1XVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
-            physicsComponent1.velocity.x = entity1XVel * sign(physicsComponent1.velocity.x);
-            if (entity1XVel < 0)
-            {
-                physicsComponent1.velocity.x = 0;
-            }
-        }
+	private void handleFriction(Entity entity1, Entity entity2, CollisionResponse collisionResponse,
+			long deltaTime)
+	{
+		switch ((collisionResponse.collisionFace))
+		{
+			case HORIZONTAL:
+				resolveHorizontalFriction(entity1, entity2, deltaTime);
+				break;
+			case HORIZONTAL_REVERSE:
+				break;
+			case VERTICAL:
+				resolveVerticalFriction(entity1, entity2, deltaTime);
+				break;
+			case VERTICAL_REVERSE:
+				break;
+		}
+	}
 
-        float entity2XVel = Math.abs(physicsComponent2.velocity.x);
-        if (entity2XVel > 0)
-        {
-            entity2XVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
-            physicsComponent2.velocity.x -= entity2XVel * sign(physicsComponent2.velocity.x);
-            if (entity2XVel < 0)
-            {
-                physicsComponent2.velocity.x = 0;
-            }
-        }
-    }
+	private void resolveHorizontalFriction(Entity entity1, Entity entity2, long deltaTime)
+	{
+		PhysicsComponent physicsComponent1 = entity1.getComponent(PhysicsComponent.class);
+		PhysicsComponent physicsComponent2 = entity2.getComponent(PhysicsComponent.class);
+		float friction =
+				physicsComponent1.friction.bottomFriction + physicsComponent2.friction.topFriction;
+		float entity1XVel = Math.abs(physicsComponent1.velocity.x);
+		if (entity1XVel > 0)
+		{
+			entity1XVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
+			physicsComponent1.velocity.x = entity1XVel * sign(physicsComponent1.velocity.x);
+			if (entity1XVel < 0)
+			{
+				physicsComponent1.velocity.x = 0;
+			}
+		}
 
-    private void resolveVerticalFriction(Entity entity1, Entity entity2, long deltaTime)
-    {
-        PhysicsComponent physicsComponent1 = entity1.getComponent(PhysicsComponent.class);
-        PhysicsComponent physicsComponent2 = entity2.getComponent(PhysicsComponent.class);
-        float friction = physicsComponent1.friction.rightFriction + physicsComponent2.friction.leftFriction;
-        float entity1YVel = Math.abs(physicsComponent1.velocity.y);
-        if (entity1YVel > 0)
-        {
-            entity1YVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
-            physicsComponent1.velocity.y = entity1YVel * sign(physicsComponent1.velocity.y);
-            if (entity1YVel < 0)
-            {
-                physicsComponent1.velocity.y = 0;
-            }
-        }
+		float entity2XVel = Math.abs(physicsComponent2.velocity.x);
+		if (entity2XVel > 0)
+		{
+			entity2XVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
+			physicsComponent2.velocity.x -= entity2XVel * sign(physicsComponent2.velocity.x);
+			if (entity2XVel < 0)
+			{
+				physicsComponent2.velocity.x = 0;
+			}
+		}
+	}
 
-        float entity2YVel = Math.abs(physicsComponent2.velocity.y);
-        if (entity2YVel > 0)
-        {
-            entity2YVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
-            physicsComponent2.velocity.y -= entity2YVel * sign(physicsComponent2.velocity.y);
-            if (entity2YVel < 0)
-            {
-                physicsComponent2.velocity.y = 0;
-            }
-        }
-    }
+	private void resolveVerticalFriction(Entity entity1, Entity entity2, long deltaTime)
+	{
+		PhysicsComponent physicsComponent1 = entity1.getComponent(PhysicsComponent.class);
+		PhysicsComponent physicsComponent2 = entity2.getComponent(PhysicsComponent.class);
+		float friction =
+				physicsComponent1.friction.rightFriction + physicsComponent2.friction.leftFriction;
+		float entity1YVel = Math.abs(physicsComponent1.velocity.y);
+		if (entity1YVel > 0)
+		{
+			entity1YVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
+			physicsComponent1.velocity.y = entity1YVel * sign(physicsComponent1.velocity.y);
+			if (entity1YVel < 0)
+			{
+				physicsComponent1.velocity.y = 0;
+			}
+		}
 
-    private float sign(float number)
-    {
-        if (number == 0.0f)
-        {
-            return 0.0f;
-        }
-        return number / Math.abs(number);
-    }
+		float entity2YVel = Math.abs(physicsComponent2.velocity.y);
+		if (entity2YVel > 0)
+		{
+			entity2YVel -= (friction * scale() * deltaTime / ONE_SECOND_NANOS);
+			physicsComponent2.velocity.y -= entity2YVel * sign(physicsComponent2.velocity.y);
+			if (entity2YVel < 0)
+			{
+				physicsComponent2.velocity.y = 0;
+			}
+		}
+	}
+
+	private float sign(float number)
+	{
+		if (number == 0.0f)
+		{
+			return 0.0f;
+		}
+		return number / Math.abs(number);
+	}
 }
