5991f0c853e888086d22c454baa84bd1e30ee1d3
Sunny <sunny.s@flipkart.com>
2018-02-22 17:08:51 +0530

    Reduce clock and dragon sprite sizes

--- FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/prefab/DragonPrefab.java
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/prefab/DragonPrefab.java
@@ -36,51 +36,61 @@ import static com.stunapps.fearlessjumper.animation.AnimationState.FLY_RIGHT;
 
 public class DragonPrefab extends Prefab
 {
-    public DragonPrefab()
-    {
-        transform = new Transform(new Position(Device.SCREEN_WIDTH / 2, 200));
+	public DragonPrefab()
+	{
+		transform = new Transform(new Position(Device.SCREEN_WIDTH / 2, 200));
 
-        StateMachine animationStateMachine = StateMachine.builder().startState(IDLE)
-                .from(IDLE).onEvent(TURN_LEFT).toState(FLY_LEFT)
-                .from(IDLE).onEvent(TURN_RIGHT).toState(FLY_RIGHT)
-                .from(FLY_LEFT).onEvent(TURN_RIGHT).toState(FLY_RIGHT)
-                .from(FLY_RIGHT).onEvent(TURN_LEFT).toState(FLY_LEFT)
-                .build();
+		StateMachine animationStateMachine =
+				StateMachine.builder().startState(IDLE).from(IDLE).onEvent(TURN_LEFT)
+						.toState(FLY_LEFT).from(IDLE).onEvent(TURN_RIGHT).toState(FLY_RIGHT)
+						.from(FLY_LEFT).onEvent(TURN_RIGHT).toState(FLY_RIGHT).from(FLY_RIGHT)
+						.onEvent(TURN_LEFT).toState(FLY_LEFT).build();
 
-        Bitmap dragonSprite1 = BitmapFactory.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly1);
-        Bitmap dragonSprite2 = BitmapFactory.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly2);
-        Bitmap dragonSprite3 = BitmapFactory.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly3);
-        Bitmap dragonSprite4 = BitmapFactory.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly4);
+		Bitmap dragonSprite1 = BitmapFactory
+				.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly1);
+		Bitmap dragonSprite2 = BitmapFactory
+				.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly2);
+		Bitmap dragonSprite3 = BitmapFactory
+				.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly3);
+		Bitmap dragonSprite4 = BitmapFactory
+				.decodeResource(Environment.CONTEXT.getResources(), R.drawable.dragon_fly4);
 
-        Animation idleAnim = new Animation(new Bitmap[]{dragonSprite1}, 0.5f);
+		Animation idleAnim = new Animation(new Bitmap[]{dragonSprite1}, 0.5f);
 
-        Animation flyRightAnim = new Animation(new Bitmap[]{dragonSprite1, dragonSprite2, dragonSprite3, dragonSprite4}, 0.5f);
+		Animation flyRightAnim = new Animation(
+				new Bitmap[]{dragonSprite1, dragonSprite2, dragonSprite3, dragonSprite4}, 0.5f);
 
-        Matrix m = new Matrix();
-        m.preScale(-1, 1);
-        dragonSprite1 = Bitmap.createBitmap(dragonSprite1, 0, 0, dragonSprite1.getWidth(), dragonSprite1.getHeight(), m, false);
-        dragonSprite2 = Bitmap.createBitmap(dragonSprite2, 0, 0, dragonSprite2.getWidth(), dragonSprite2.getHeight(), m, false);
-        dragonSprite3 = Bitmap.createBitmap(dragonSprite3, 0, 0, dragonSprite3.getWidth(), dragonSprite3.getHeight(), m, false);
-        dragonSprite4 = Bitmap.createBitmap(dragonSprite4, 0, 0, dragonSprite4.getWidth(), dragonSprite4.getHeight(), m, false);
-        Animation flyLeftAnim = new Animation(new Bitmap[]{dragonSprite1, dragonSprite2, dragonSprite3, dragonSprite4}, 0.5f);
+		Matrix m = new Matrix();
+		m.preScale(-1, 1);
+		dragonSprite1 = Bitmap.createBitmap(dragonSprite1, 0, 0, dragonSprite1.getWidth(),
+				dragonSprite1.getHeight(), m, false);
+		dragonSprite2 = Bitmap.createBitmap(dragonSprite2, 0, 0, dragonSprite2.getWidth(),
+				dragonSprite2.getHeight(), m, false);
+		dragonSprite3 = Bitmap.createBitmap(dragonSprite3, 0, 0, dragonSprite3.getWidth(),
+				dragonSprite3.getHeight(), m, false);
+		dragonSprite4 = Bitmap.createBitmap(dragonSprite4, 0, 0, dragonSprite4.getWidth(),
+				dragonSprite4.getHeight(), m, false);
+		Animation flyLeftAnim = new Animation(
+				new Bitmap[]{dragonSprite1, dragonSprite2, dragonSprite3, dragonSprite4}, 0.5f);
 
-        Map<AnimationState, Animation> stateAnimationMap = new HashMap<>();
-        stateAnimationMap.put(IDLE, idleAnim);
-        stateAnimationMap.put(FLY_RIGHT, flyRightAnim);
-        stateAnimationMap.put(FLY_LEFT, flyLeftAnim);
+		Map<AnimationState, Animation> stateAnimationMap = new HashMap<>();
+		stateAnimationMap.put(IDLE, idleAnim);
+		stateAnimationMap.put(FLY_RIGHT, flyRightAnim);
+		stateAnimationMap.put(FLY_LEFT, flyLeftAnim);
 
-        Animator animator =
-                new Animator(stateAnimationMap, Delta.ZERO, dragonSprite1.getWidth(),
-                        dragonSprite1.getHeight(), animationStateMachine);
-//        animator.triggerEvent(TURN_RIGHT);
-        components.add(animator);
-        components.add(new Dragon());
-        components.add(new Obstacle());
-        components.add(new RectCollider(Delta.ZERO, dragonSprite1.getWidth(),
-                dragonSprite1.getHeight()));
-        components.add(new PeriodicTranslation()
-                .withXMovement(0, Device.SCREEN_WIDTH - dragonSprite1.getWidth(), 5f));
-        components.add(new ContactDamageComponent(1));
-        components.add(new PhysicsComponent(Float.MAX_VALUE, new PhysicsComponent.Velocity(), false));
-    }
+		Animator animator = new Animator(stateAnimationMap, Delta.ZERO, dragonSprite1.getWidth(),
+				dragonSprite1.getHeight(), animationStateMachine);
+		//        animator.triggerEvent(TURN_RIGHT);
+		components.add(animator);
+		components.add(new Dragon());
+		components.add(new Obstacle());
+		components.add(new RectCollider(Delta.ZERO, dragonSprite1.getWidth(),
+				dragonSprite1.getHeight()));
+		components.add(new PeriodicTranslation()
+				.withXMovement(0, Device.SCREEN_WIDTH - dragonSprite1.getWidth(), 5f));
+		components.add(new ContactDamageComponent(1));
+		components
+				.add(new PhysicsComponent(Float.MAX_VALUE, new PhysicsComponent.Velocity(),
+						false));
+	}
 }
