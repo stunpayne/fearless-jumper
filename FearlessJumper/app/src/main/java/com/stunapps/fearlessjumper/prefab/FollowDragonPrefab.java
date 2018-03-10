6c479ab23f65102edf740926cad56db7e4587a7b
Anand Verma <anand.verma@flipkart.com>
2018-03-10 21:38:16 +0530

    Add follow dragon prefab

---
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/prefab/FollowDragonPrefab.java
@@ -0,0 +1,13 @@
+package com.stunapps.fearlessjumper.prefab;
+
+/**
+ * Created by anand.verma on 10/03/18.
+ */
+
+public class FollowDragonPrefab extends Prefab
+{
+	public FollowDragonPrefab()
+	{
+
+	}
+}
