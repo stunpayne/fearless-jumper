a1a8db3d20745b2951729f031570978b6004b76c
Anand Verma <anand.verma@flipkart.com>
2018-03-11 09:02:05 +0530

    rebase on master latest

--- FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/prefab/FollowDragonPrefab.java
+++ FearlessJumper/app/src/main/java/com/stunapps/fearlessjumper/prefab/FollowDragonPrefab.java
@@ -1,5 +1,9 @@
 package com.stunapps.fearlessjumper.prefab;
 
+import com.stunapps.fearlessjumper.component.Component;
+
+import java.util.List;
+
 /**
  * Created by anand.verma on 10/03/18.
  */
@@ -9,5 +13,17 @@ public class FollowDragonPrefab extends Prefab
 	public FollowDragonPrefab()
 	{
 
+	}
+
+	@Override
+	public List<Instantiable> getInstantiables()
+	{
+		return null;
+	}
+
+	@Override
+	public <C extends Component> C getComponent(Class<C> componentType)
+	{
+		return null;
 	}
 }
